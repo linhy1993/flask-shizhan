a307b3a96d25c7e46ec36fa7977e6188905042dd
linhy1993 <linhongyi1110@gmail.com>
2019-09-27 15:19:35 -0400

    change templates

--- movies/views.py
+++ movies/views.py
@@ -6,14 +6,10 @@ from movies.models import MaoyanMovie, clean_db, init_db
 mod = Blueprint('movie', __name__)
 
 
+
 @mod.route('/')
-def index():
-    return render_template('index.html')
-
-
-@mod.route('/movies/')
 def movies():
     page = request.args.get('page', 1, type=int)
     pagination = MaoyanMovie.query.paginate(page, per_page=8, error_out=False)
     movie_shown = pagination.items
-    return render_template('movies.html', movie_shown=movie_shown, pagination=pagination)
+    return render_template('index.html', movie_shown=movie_shown, pagination=pagination)
