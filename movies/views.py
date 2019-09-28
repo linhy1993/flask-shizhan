3cacaeb5cf50bff5612bf067df0963327a1ed70c
linhy1993 <linhongyi1110@gmail.com>
2019-09-28 12:12:09 -0400

    add pagination

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, request
-from movies import db
-from movies.models import MaoyanMovie, clean_db, init_db
+
+from movies.models import MaoyanMovie
 
 mod = Blueprint('movie', __name__)
-
 
 
 @mod.route('/')
 def movies():
     page = request.args.get('page', 1, type=int)
     pagination = MaoyanMovie.query.paginate(page, per_page=8, error_out=False)
+    movie_shown = pagination.items
+    return render_template('index.html', movie_shown=movie_shown, pagination=pagination)
+
+
+@mod.route('/<int:page>/')
+def movie_pages(page=None):
+    if page is None:
+        page = 1
+    pagination = MaoyanMovie.query.paginate(page=page, per_page=8)
     movie_shown = pagination.items
     return render_template('index.html', movie_shown=movie_shown, pagination=pagination)
